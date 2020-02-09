5e4624139483280189b643768b2180758de5a8bc
Gaurav <gkgarry911@gmail.com>
2020-02-09 20:15:31 +0530

    sunday update meranda

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './subparts/header/header.component';
+import { FooterComponent } from './subparts/footer/footer.component';
+import { MainbodyComponent } from './subparts/mainbody/mainbody.component';
+import { CarouselComponent } from './subparts/mainbody/carousel/carousel.component';
+import { EditorsPickComponent } from './subparts/mainbody/editors-pick/editors-pick.component';
+import { TrendingComponent } from './subparts/mainbody/trending/trending.component';
+import { RecentNewsComponent } from './subparts/mainbody/recent-news/recent-news.component';
+import { PopularPostsComponent } from './subparts/mainbody/popular-posts/popular-posts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainbodyComponent,
+    CarouselComponent,
+    EditorsPickComponent,
+    TrendingComponent,
+    RecentNewsComponent,
+    PopularPostsComponent
   ],
   imports: [
     BrowserModule,
